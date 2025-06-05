d7cdf4a7237eb5324c5f73155279d9acbee20452
Bharat Ram <bharatsarma2512@gmail.com>
2025-06-05 07:23:02 +0530

    palindromic substring

---
+++ palindromic substring/main.py
@@ -0,0 +1,11 @@
+def palindromic_substring(str):
+    count=0
+    str1=" "
+    for i in range(len(str)):
+        for j in range(i, len(str)):
+            ss=str[i:j+1]
+            if ss==ss[::-1]:
+                str1+=ss
+        return len(set(str1))
+str=input("Enter a string: ")
+print("Number of unique palindromic substrings:", palindromic_substring(str))
