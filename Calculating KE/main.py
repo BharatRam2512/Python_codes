26d8be737fb261442b906e30a135751b46be1e58
Bharat Ram <bharatsarma2512@gmail.com>
2025-06-05 08:37:13 +0530

    calculating Kinetic Energy

---
+++ Calculating KE/main.py
@@ -0,0 +1,4 @@
+mass=int(input("Enter the mass of the object in kg: "))
+velocity=int(input("Enter the velocity of the object in m/s: ")) 
+KE=0.5 * mass * velocity ** 2
+print("The momentum of the object is:", KE, "kg m/s")
