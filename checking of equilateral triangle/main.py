5f2d99a4eedd7642f27050448af2dc693e90955a
Bharat Ram <bharatsarma2512@gmail.com>
2025-06-05 10:52:15 +0530

    checking a triangle an equilateral or not

---
+++ checking of equilateral triangle/main.py
@@ -0,0 +1,7 @@
+side1=int(input("Enter the length of the first side: "))
+side2=int(input("Enter the length of the second side: "))
+side3=int(input("Enter the length of the third side: "))
+if side1==side2==side3:
+    print("The triangle is equilateral.")
+else:
+    print("The triangle is not equilateral.")   
